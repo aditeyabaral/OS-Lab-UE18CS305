f1683d9489fde573ed1b14e83dc711771e0be28e
Aditeya Baral <aditeya.baral@gmail.com>
2020-09-08 15:58:29 +0530

    Added scripts

---
+++ PES1201800366 Aditeya Baral W2/echo.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo -e "hello \\ bye"
+echo -e "hello \r bye"
+echo -e "hello \n bye"
+echo -e "hello \c bye"
+echo -e "hello \b bye"
+echo -e "hello \a bye"
+echo -e "An apple a day keeps away \a\t\tdoctor\n"
---
+++ PES1201800366 Aditeya Baral W2/sysvar.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo $BASH
+echo $BASH_VERSION
+echo $COLUMNS
+echo $HOME
+echo $LINES
+echo $LOGNAME
+echo $OSTYPE
+echo $PATH
+echo $PS1
+echo $PWD
+echo $SHELL
+echo $USERNAME
